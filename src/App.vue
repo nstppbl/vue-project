0b2f4ac4d76d976dc562ac8e4fc692f81f490d73
nstppbl <1nstppbl@gmail.com>
2021-08-09 20:24:25 +0500

    finish layout and styling

--- src/App.vue
+++ src/App.vue
@@ -6,36 +6,75 @@
           to-do list
         </div>
         <div class="todo-body">
-            <div class="todo-item todo-item--active">
+            <div class="todo-task todo-task--active">
               <label class="checkbox-container">
                 <span class="checkbox-label">Task 1</span>
                 <input type="checkbox" checked="checked">
                 <span class="checkmark"></span>
                 <span class="checkmark-border"></span>
               </label>
-              <a href="#" class="close-button todo-item__close-button">
+              <a href="#" class="close-button todo-task__close-button">
                 <img src="@/assets/icons/close-icon.svg" alt="close-icon">
               </a>
             </div>
-            <div class="todo-item todo-item--active">
+            <div class="todo-subtask todo-subtask--active">
+              <label class="checkbox-container">
+                <span class="checkbox-label">Subtask 1</span>
+                <input type="checkbox" checked="checked">
+                <span class="checkmark"></span>
+                <span class="checkmark-border"></span>
+              </label>
+              <a href="#" class="close-button todo-task__close-button">
+                <img src="@/assets/icons/close-icon.svg" alt="close-icon">
+              </a>
+            </div>
+            <div class="todo-subtask">
+              <label class="checkbox-container">
+                <span class="checkbox-label">Subtask 2</span>
+                <input type="checkbox" checked="checked">
+                <span class="checkmark"></span>
+                <span class="checkmark-border"></span>
+              </label>
+              <a href="#" class="close-button todo-task__close-button">
+                <img src="@/assets/icons/close-icon.svg" alt="close-icon">
+              </a>
+            </div>
+            <div class="todo-subtask">
+              <label class="checkbox-container">
+                <span class="checkbox-label">Subtask 3</span>
+                <input type="checkbox" checked="checked">
+                <span class="checkmark"></span>
+                <span class="checkmark-border"></span>
+              </label>
+              <a href="#" class="close-button todo-task__close-button">
+                <img src="@/assets/icons/close-icon.svg" alt="close-icon">
+              </a>
+            </div>
+            <div class="add-subtask">
+              <a href="#" class="add-button todo-subtask__add-button">
+                <img src="@/assets/icons/add-icon.png" alt="add-icon">
+              </a>
+            <input class="add-subtask-input" type="text" name="subtask" placeholder="Add a new task">
+            </div>
+            <div class="todo-task">
               <label class="checkbox-container">
                 <span class="checkbox-label">Task 2</span>
                 <input type="checkbox">
                 <span class="checkmark"></span>
                 <span class="checkmark-border"></span>
               </label>
-              <a href="#" class="close-button todo-item__close-button">
+              <a href="#" class="close-button todo-task__close-button">
                 <img src="@/assets/icons/close-icon.svg" alt="close-icon">
               </a>
             </div>
-            <div class="todo-item todo-item--active">
+            <div class="todo-task">
               <label class="checkbox-container">
                 <span class="checkbox-label">Task 3</span>
                 <input type="checkbox">
                 <span class="checkmark"></span>
                 <span class="checkmark-border"></span>
               </label>
-              <a href="#" class="close-button todo-item__close-button">
+              <a href="#" class="close-button todo-task__close-button">
                 <img src="@/assets/icons/close-icon.svg" alt="close-icon">
               </a>
             </div>
@@ -44,17 +83,17 @@
         <div class="todo-footer">
           <div class="task-qty">1/3 left</div>
           <div class="task-status">
-            <label class="container">All
+            <label class="status-container">All
               <input type="radio" checked="checked" name="radio">
-              <span class="checkmark-radio-all"></span>
+              <span class="checkmark-radio checkmark-radio-all"></span>
               </label>
-              <label class="container">Active
+              <label class="status-container">Active
               <input type="radio" name="radio">
-              <span class="checkmark-radio-active"></span>
+              <span class="checkmark-radio checkmark-radio-active"></span>
               </label>
-             <label class="container">Completed
+             <label class="status-container">Completed
               <input type="radio" name="radio">
-              <span class="checkmark-radio-completed"></span>
+              <span class="checkmark-radio checkmark-radio-completed"></span>
             </label>
           </div>
         </div>
@@ -95,7 +134,7 @@ body {
     color: $text-color-dark;
 }
 
-.todo-item {
+.todo-task {
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -103,10 +142,32 @@ body {
     height: 50px;
     background-color: $base-color-light;
     color: $text-color-dark;
-    margin: 30px auto;
+    margin: 0px auto;
+    margin-top: 30px;
     padding-left: 20px;
     padding-right: 20px;
     border-radius: 10px;
+}
+
+.todo-task--active {
+  border-radius: 10px 10px 0 0;
+}
+
+
+
+.todo-subtask {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 480px;
+    height: 50px;
+    background-color: $base-color-pale;
+    color: $text-color-dark;
+    margin: 0px auto;
+    padding-left: 20px;
+    padding-right: 20px;
+    border: 1px solid $base-color-light;
+    border-top: 0;
 }
 
 .todo-button {
@@ -114,6 +175,7 @@ body {
     height: 50px;
     background-color: $base-color-pale;
     margin-bottom: 30px;
+    margin-top: 30px;
     border-radius: 10px;
     border-style: dashed;
     border-width: 2px;
@@ -144,7 +206,31 @@ body {
     color: $text-color-light;
 }
 
+.add-subtask {
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    width: 480px;
+    height: 50px;
+    background-color: $base-color-pale;
+    color: $text-color-dark;
+    margin: 0px auto;
+    padding-left: 20px;
+    padding-right: 20px;
+    border-radius: 0 0 10px 10px;
+    border: 1px solid $base-color-light;
+    border-top: 0;
+}
 
+.add-subtask-input {
+    margin-left: 10px;
+    background-color: $base-color-pale;
+    color: $text-color-dark;
+}
+
+.add-subtask-input::placeholder {
+    color: $text-color-light;
+}
 
 
 </style>
